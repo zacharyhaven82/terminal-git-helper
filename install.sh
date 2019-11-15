ff2e9abd459ee836d71e9dcce34e370f6137f671
Zachary Haven <>
2019-11-15 09:01:02 -0600

    Make copy interactive

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
-\cp dot_bash_profile ~/.bash_profile
-\cp dot_bash_profile_tilda ~/.bash_profile~
-
+cp -i dot_bash_profile ~/.bash_profile
+cp -i dot_bash_profile_tilda ~/.bash_profile~
 
 \cp git-completion.sh ~/.git-completion.sh
 \cp git-prompt.sh ~/.git-prompt.sh
