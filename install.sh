2e730bee8633f8ab4bf3cad09f7f9af5cde64776
Zachary Haven <>
2019-11-15 11:43:48 -0600

    Make copy interactive again

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
+echo "Installing Terminal Git Helper"
 cp -i dot_bash_profile ~/.bash_profile
 cp -i dot_bash_profile_tilda ~/.bash_profile~
 
 \cp git-completion.sh ~/.git-completion.sh
 \cp git-prompt.sh ~/.git-prompt.sh
+echo "Complete"
